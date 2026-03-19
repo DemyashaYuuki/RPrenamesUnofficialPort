a04f7ae0146a3bf5c22452232c57f439ff1fdab5
DemyashaYuuki <demyan.panov2@gmail.com>
2026-03-19 19:16:03 +0000

    Add HandledScreenAccessor mixin for x and y accessors

---
+++ src/main/java/com/hiword9/rprenames/client/mixin/HandledScreenAccessor.java
@@ -0,0 +1,14 @@
+package com.hiword9.rprenames.client.mixin;
+
+import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(HandledScreen.class)
+public interface HandledScreenAccessor {
+    @Accessor("x")
+    int rprenames$getX();
+
+    @Accessor("y")
+    int rprenames$getY();
+}
